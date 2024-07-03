042e93d8473091b7be1b03dbefc3a17909cab3d3
Packie <Packie.yy.bi@viu.com>
2024-07-03 13:46:22 +0800

    Confif: update the favicon url

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
 	title: '每週記事',
-	author: 'Liam',
-	description: '記錄全新 Liam 的每週趣事',
+	author: 'Mr.Pat',
+	description: '記錄 Mr.Pat 的每週趣事',
 	keywords: 'Liam,Weekly',
-	icon: 'https://gw.alicdn.com/imgextra/i2/O1CN01m9YYjS1QBeW5DOm3I_!!6000000001938-2-tps-400-400.png',
+	icon: 'https://gz-blog-storage-1252787757.cos.ap-guangzhou.myqcloud.com/public/weekly-512x512.png',
 	pic: 'https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png',
 	homePage: 'https://weekly.biyongyao.com',
 	blogPage: 'https://biyongyao.com',
