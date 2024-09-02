ee8af81a051e1fc7f4fee67ab4e8e03b06e02cb0
Packie <Packie.yy.bi@viu.com>
2024-09-02 18:01:12 +0800

    config: init config
    config: init config
    
    config: the article format

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -13,9 +13,9 @@ export function GET() {
   posts = posts.slice(0, 12);
 
   return rss({
-    title: '潮流周刊',
-    description: '记录工程师 Tw93 的不枯燥生活',
-    site: 'https://weekly.tw93.fun/',
+    title: '每週趣事',
+    description: '記錄 Liam 的每週趣事',
+    site: 'https://weekly.biyongyao.com/',
     customData: `<image><url>https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png</url></image>`,
     items: posts.map((item) => {
       const [issueNumber, issueTitle] = item.url.split('/posts/')[1].split('-');
