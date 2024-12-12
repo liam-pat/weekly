fe50ab2f8f6f119389578b958e5d1d13ffaf2b0a
Packie <Packie.yy.bi@viu.com>
2024-12-12 14:50:12 +0800

    feat: rss auth
    feat: rss auth

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -14,9 +14,9 @@ export function GET() {
 
   return rss({
     title: '每週趣事',
-    description: '記錄 Liam 的每週趣事',
+    description: '記錄 Liam 每週趣事',
     site: 'https://weekly.biyongyao.com/',
-    customData: `<image><url>https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png</url></image>`,
+    customData: `<image><url>https://gz-blog-storage-1252787757.cos.ap-guangzhou.myqcloud.com/public/weekly-512x512.png</url></image><follow_challenge><feedId>83723980500419584</feedId><userId>83722505120690176</userId></follow_challenge>`,
     items: posts.map((item) => {
       const [issueNumber, issueTitle] = item.url.split('/posts/')[1].split('-');
       const title = `第${issueNumber}期 - ${issueTitle}`;
